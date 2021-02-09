eabdb45344bbbbdc429561a9cc9fd74c807a1aa6
Dorkt <gabrieldorkt@hotmail.com>
2021-02-09 16:18:22 -0300

    first commit

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,14 @@
 const mongoose = require ('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/noderest', { 
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(res => console.log("Banco de dados conectado"))
+.catch(err => console.log("Error:", err));
+
+
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
